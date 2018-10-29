1e4bd15b9ff01ee9fc8268426d373080c86be06f
tl00456 <oohty15@gmail.com>
2018-10-29 14:03:45 -0700

    Lab 1 Almost Draft

--- app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
+++ app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CardTest {
-
+    Card[] one = {
+            new Card(Card.Shape.NOSHAPE, Card.Color.NOCOLOR, 0, 0, R.drawable.circle_blue_1_0, R.drawable.circle_blue_2_1_selected),
+            new Card(Card.Shape.TRIANGLE, Card.Color.RED, 1, 1, R.drawable.square_green_1_1, R.drawable.square_green_3_0_selected),
+            new Card(Card.Shape.CIRCLE, Card.Color.BLUE, 2, 2, R.drawable.triangle_red_3_2, R.drawable.triangle_red_1_2_selected),
+            new Card(Card.Shape.SQUARE, Card.Color.GREEN, 3, 3, R.drawable.triangle_red_3_2, R.drawable.triangle_red_1_2_selected)};
     @Test
     public void getShape() {
-        Card[] one = {
-                new Card(Card.Shape.NOSHAPE, Card.Color.GREEN, 0, 0, 0, 0),
-                new Card(Card.Shape.TRIANGLE, Card.Color.GREEN, 0, 0, 0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, 0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, 0, 0)};
         Card.Shape[] t = {Card.Shape.values()[0], Card.Shape.values()[1], Card.Shape.values()[2], Card.Shape.values()[3]};
         Card.Shape[] s = new Card.Shape[4];
         for (int i = 0; i < one.length; i++) {
@@ -23,11 +22,6 @@ public class CardTest {
 
     @Test
     public void getColor() {
-        Card[] one = {
-                new Card(Card.Shape.NOSHAPE, Card.Color.NOCOLOR, 0, 0, 0, 0),
-                new Card(Card.Shape.TRIANGLE, Card.Color.RED, 0, 0, 0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.BLUE, 0, 0, 0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, 0, 0)};
         Card.Color[] t = {Card.Color.values()[0], Card.Color.values()[1], Card.Color.values()[2], Card.Color.values()[3]};
         Card.Color[] s = new Card.Color[4];
         for (int i = 0; i < one.length; i++) {
@@ -38,11 +32,6 @@ public class CardTest {
 
     @Test
     public void getNumber() {
-        Card[] one = {
-                new Card(Card.Shape.NOSHAPE, Card.Color.GREEN, 0, 0, 0, 0),
-                new Card(Card.Shape.TRIANGLE, Card.Color.GREEN, 1, 0, 0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 2, 0, 0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 3, 0, 0, 0)};
         int[] t = {0, 1, 2, 3};
         int[] s = new int[4];
         for (int i = 0; i < one.length; i++) {
@@ -53,11 +42,6 @@ public class CardTest {
 
     @Test
     public void getAlpha() {
-        Card[] one = {
-                new Card(Card.Shape.NOSHAPE, Card.Color.GREEN, 0, 0, 0, 0),
-                new Card(Card.Shape.TRIANGLE, Card.Color.GREEN, 0, 1, 0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 2, 0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 3, 0, 0)};
         int[] t = {0, 1, 2, 3};
         int[] s = new int[4];
         for (int i = 0; i < one.length; i++) {
@@ -69,109 +53,32 @@ public class CardTest {
 
     @Test
     public void getImageID() {
-    Card[] one = {
-                new Card(Card.Shape.NOSHAPE, Card.Color.GREEN, 0,0, R.drawable.blank_0_0_0, 0),
-                new Card(Card.Shape.NOSHAPE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_1_0, 0),
-                new Card(Card.Shape.TRIANGLE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_2_0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_2_1, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_blue_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_2_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_2_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_green_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_2_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_2_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.circle_red_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_2_0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_2_1, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_blue_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_2_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_2_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_green_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_2_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_2_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.square_red_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_2_0, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_2_1, 0),
-                new Card(Card.Shape.SQUARE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_blue_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_2_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_2_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_green_3_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_1_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_1_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_1_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_2_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_2_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_2_2, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_3_0, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_3_1, 0),
-                new Card(Card.Shape.CIRCLE, Card.Color.GREEN, 0, 0, R.drawable.triangle_red_3_2, 0)};
- //       Card.Shape[] t = {Card.Shape.values()[0], Card.Shape.values()[1], Card.Shape.values()[2], Card.Shape.values()[3]};
-     //   Card.Shape[] s = new Card.Shape[62];
-    //    for (int i = 0; i < one.length; i++) {
-   //         s[i] = one[i].getImageID();
-   //     }
-    //    assertArrayEquals(s, t);
+        int[] t = {R.drawable.circle_blue_1_0, R.drawable.square_green_1_1, R.drawable.triangle_red_3_2};
+        int[] s = new int[3];
+        for (int i = 0; i < one.length; i++) {
+            s[i] = one[i].getImageID();
+        }
+        assertArrayEquals(s, t);
     }
-/*
+
     @Test
     public void getSelectedImageID() {
+        int[] t = {R.drawable.circle_blue_2_1_selected, R.drawable.square_green_3_0_selected, R.drawable.triangle_red_1_2_selected};
+        int[] s = new int[3];
+        for(int i = 0; i < one.length;i++) {
+            s[i] = one[i].getSelectedImageID();
+        }
+        assertArrayEquals(s, t);
     }
 
     @Test
-    public void getIsSelected() {
+    public void setAndGetIsSelected(){
+        one[1].getSelectedImageID();
+        one[2].getSelectedImageID();
+        boolean[] expected = {true, true, false};
+        boolean[] reality = {true, true, false};
+        assertArrayEquals(expected, reality);
     }
-
-    @Test
-    public void setIsSelected() {
-    }
-*/
 }
 
 
