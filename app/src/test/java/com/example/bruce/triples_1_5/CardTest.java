4b2b2eedf5988d143829699559bab09269479be1
WooziLover <oohty15@gmail.com>
2018-10-29 19:56:28 -0700

    Lab 1 is complete and ready for grading

--- app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
+++ app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CardTest {
+
     Card[] one = {
             new Card(Card.Shape.NOSHAPE, Card.Color.NOCOLOR, 0, 0, R.drawable.circle_blue_1_0, R.drawable.circle_blue_2_1_selected),
             new Card(Card.Shape.TRIANGLE, Card.Color.RED, 1, 1, R.drawable.square_green_1_1, R.drawable.square_green_3_0_selected),
             new Card(Card.Shape.CIRCLE, Card.Color.BLUE, 2, 2, R.drawable.triangle_red_3_2, R.drawable.triangle_red_1_2_selected),
-            new Card(Card.Shape.SQUARE, Card.Color.GREEN, 3, 3, R.drawable.triangle_red_3_2, R.drawable.triangle_red_1_2_selected)};
+            new Card(Card.Shape.SQUARE, Card.Color.GREEN, 3, 3, R.drawable.blank_0_0_0, R.drawable.circle_green_2_0_selected)};
+
     @Test
     public void getShape() {
         Card.Shape[] t = {Card.Shape.values()[0], Card.Shape.values()[1], Card.Shape.values()[2], Card.Shape.values()[3]};
@@ -53,8 +55,8 @@ public class CardTest {
 
     @Test
     public void getImageID() {
-        int[] t = {R.drawable.circle_blue_1_0, R.drawable.square_green_1_1, R.drawable.triangle_red_3_2};
-        int[] s = new int[3];
+        int[] t = {R.drawable.circle_blue_1_0, R.drawable.square_green_1_1, R.drawable.triangle_red_3_2, R.drawable.blank_0_0_0};
+        int[] s = new int[4];
         for (int i = 0; i < one.length; i++) {
             s[i] = one[i].getImageID();
         }
@@ -63,13 +65,17 @@ public class CardTest {
 
     @Test
     public void getSelectedImageID() {
-        int[] t = {R.drawable.circle_blue_2_1_selected, R.drawable.square_green_3_0_selected, R.drawable.triangle_red_1_2_selected};
-        int[] s = new int[3];
+        int[] t = {R.drawable.circle_blue_2_1_selected, R.drawable.square_green_3_0_selected, R.drawable.triangle_red_1_2_selected, R.drawable.circle_green_2_0_selected};
+        int[] s = new int[4];
         for(int i = 0; i < one.length;i++) {
             s[i] = one[i].getSelectedImageID();
         }
         assertArrayEquals(s, t);
     }
+
+    /*************************************************
+     * I combined the setIsSelected and getIsSelected
+     ************************************************/
 
     @Test
     public void setAndGetIsSelected(){
