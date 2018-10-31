4eb863e762bad5816dfa9686ab178fc236f7a09f
tl00456 <oohty15@gmail.com>
2018-10-31 13:48:13 -0700

    Revised from Lab 1 correcting comments

--- app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
+++ app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class CardTest {
 
-    Card[] one = {
+    Card[] card = {
             new Card(Card.Shape.NOSHAPE, Card.Color.NOCOLOR, 0, 0, R.drawable.circle_blue_1_0, R.drawable.circle_blue_2_1_selected),
             new Card(Card.Shape.TRIANGLE, Card.Color.RED, 1, 1, R.drawable.square_green_1_1, R.drawable.square_green_3_0_selected),
             new Card(Card.Shape.CIRCLE, Card.Color.BLUE, 2, 2, R.drawable.triangle_red_3_2, R.drawable.triangle_red_1_2_selected),
@@ -14,63 +14,63 @@ public class CardTest {
 
     @Test
     public void getShape() {
-        Card.Shape[] t = {Card.Shape.values()[0], Card.Shape.values()[1], Card.Shape.values()[2], Card.Shape.values()[3]};
-        Card.Shape[] s = new Card.Shape[4];
-        for (int i = 0; i < one.length; i++) {
-            s[i] = one[i].getShape();
+        Card.Shape[] actualValue = {Card.Shape.values()[0], Card.Shape.values()[1], Card.Shape.values()[2], Card.Shape.values()[3]};
+        Card.Shape[] expectedValue = new Card.Shape[4];
+        for (int i = 0; i < card.length; i++) {
+            expectedValue[i] = card[i].getShape();
         }
-        assertArrayEquals(s, t);
+        assertArrayEquals(expectedValue, actualValue);
     }
 
     @Test
     public void getColor() {
-        Card.Color[] t = {Card.Color.values()[0], Card.Color.values()[1], Card.Color.values()[2], Card.Color.values()[3]};
-        Card.Color[] s = new Card.Color[4];
-        for (int i = 0; i < one.length; i++) {
-            s[i] = one[i].getColor();
+        Card.Color[] actualValue = {Card.Color.values()[0], Card.Color.values()[1], Card.Color.values()[2], Card.Color.values()[3]};
+        Card.Color[] expectedValue = new Card.Color[4];
+        for (int i = 0; i < card.length; i++) {
+            expectedValue[i] = card[i].getColor();
         }
-        assertArrayEquals(s, t);
+        assertArrayEquals(expectedValue, actualValue);
     }
 
     @Test
     public void getNumber() {
-        int[] t = {0, 1, 2, 3};
-        int[] s = new int[4];
-        for (int i = 0; i < one.length; i++) {
-            s[i] = one[i].getNumber();
+        int[] actualValue = {0, 1, 2, 3};
+        int[] expectedValue = new int[4];
+        for (int i = 0; i < card.length; i++) {
+            expectedValue[i] = card[i].getNumber();
         }
-        assertArrayEquals(s, t);
+        assertArrayEquals(expectedValue, actualValue);
     }
 
     @Test
     public void getAlpha() {
-        int[] t = {0, 1, 2, 3};
-        int[] s = new int[4];
-        for (int i = 0; i < one.length; i++) {
-            s[i] = one[i].getAlpha();
+        int[] actualValue = {0, 1, 2, 3};
+        int[] expectedValue = new int[4];
+        for (int i = 0; i < card.length; i++) {
+            expectedValue[i] = card[i].getAlpha();
         }
-        assertArrayEquals(s, t);
+        assertArrayEquals(expectedValue, actualValue);
     }
 
 
     @Test
     public void getImageID() {
-        int[] t = {R.drawable.circle_blue_1_0, R.drawable.square_green_1_1, R.drawable.triangle_red_3_2, R.drawable.blank_0_0_0};
-        int[] s = new int[4];
-        for (int i = 0; i < one.length; i++) {
-            s[i] = one[i].getImageID();
+        int[] actualValue = {R.drawable.circle_blue_1_0, R.drawable.square_green_1_1, R.drawable.triangle_red_3_2, R.drawable.blank_0_0_0};
+        int[] expectedValue = new int[4];
+        for (int i = 0; i < card.length; i++) {
+            expectedValue[i] = card[i].getImageID();
         }
-        assertArrayEquals(s, t);
+        assertArrayEquals(expectedValue, actualValue);
     }
 
     @Test
     public void getSelectedImageID() {
-        int[] t = {R.drawable.circle_blue_2_1_selected, R.drawable.square_green_3_0_selected, R.drawable.triangle_red_1_2_selected, R.drawable.circle_green_2_0_selected};
-        int[] s = new int[4];
-        for(int i = 0; i < one.length;i++) {
-            s[i] = one[i].getSelectedImageID();
+        int[] actualValue = {R.drawable.circle_blue_2_1_selected, R.drawable.square_green_3_0_selected, R.drawable.triangle_red_1_2_selected, R.drawable.circle_green_2_0_selected};
+        int[] expectedValue = new int[4];
+        for(int i = 0; i < card.length;i++) {
+            expectedValue[i] = card[i].getSelectedImageID();
         }
-        assertArrayEquals(s, t);
+        assertArrayEquals(expectedValue, actualValue);
     }
 
     /*************************************************
@@ -79,13 +79,13 @@ public class CardTest {
 
     @Test
     public void setAndGetIsSelected(){
-        one[1].setIsSelected(false);
-        one[2].setIsSelected(false);
-        one[1].getSelectedImageID();
-        one[2].getSelectedImageID();
-        boolean[] expected = {true, true, false};
-        boolean[] reality = {true, true, false};
-        assertArrayEquals(expected, reality);
+        card[1].setIsSelected(false);
+        card[2].setIsSelected(false);
+        card[1].getSelectedImageID();
+        card[2].getSelectedImageID();
+        boolean[] expectedValue = {true, true, false};
+        boolean[] actualValue = {true, true, false};
+        assertArrayEquals(expectedValue, actualValue);
     }
 }
 
